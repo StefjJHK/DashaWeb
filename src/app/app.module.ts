d4f6b9fc7125ef1334151f540b1175f5840e3cb6
Sergey_Stef <sergey.stef@iproject.by>
2023-06-18 00:45:10 +0300

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { LOCALE_ID, NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { UserModule } from "./user/user.module";
+import { AuthorizationModule } from "./authorization/authorization.module";
+import { SharedModule } from "./shared/shared.module";
+import { HttpClientModule } from "@angular/common/http"
+import { AdminModule } from "./admin/admin.module";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AdminModule,
+    SharedModule,
+    AppRoutingModule,
+    UserModule,
+    AuthorizationModule,
+    HttpClientModule,
+  ],
+  providers: [
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
